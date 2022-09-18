7dff66834626d719f0083c9d935ec7887b6be4cc
Jamgle <jamgle91@163.com>
2022-09-18 16:38:19 +0800

    login校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -9,6 +9,9 @@
           v-model="user.mobile"
           name="手机号"
           placeholder="请输入手机号"
+          :rules="userFormRules.mobile"
+          type="number"
+          maxlength="11"
       >
         <i slot="left-icon" class="toutiao toutiao-shouji"/>
       </van-field>
@@ -16,10 +19,19 @@
           v-model="user.code"
           name="验证码"
           placeholder="请输入验证码"
+          :rules="userFormRules.code"
+          type="number"
+          maxlength="6"
       >
         <i slot="left-icon" class="toutiao toutiao-yanzhengma"/>
         <template #button>
-          <van-button class="send-sms-btn" round size="small" type="default">发送验证码</van-button>
+          <van-button
+              class="send-sms-btn"
+              round size="small"
+              type="default"
+              @click="onSendSms"
+              native-type="button"
+          >发送验证码</van-button>
         </template>
       </van-field>
       <div class="login-btn-wrap">
@@ -42,6 +54,28 @@ export default {
       user: {
         mobile: '',
         code: ''
+      },
+      userFormRules: {
+        mobile:[
+          {
+            required: true,
+            message: '手机号码不能为空'
+          },
+          {
+            pattern: /^1[3|5|7|8|9]\d{9}$/,
+            message: '手机号码格式不正确'
+          }
+        ],
+        code:[
+          {
+            required: true,
+            message: '验证码不能为空'
+          },
+          {
+            pattern: /^\d{6}$/,
+            message: '验证码格式不正确'
+          }
+        ]
       }
     }
   },
@@ -51,14 +85,32 @@ export default {
   mounted () {},
   methods: {
     async onSubmit() {
+      // 1.获取表单数据
       const user = this.user
+
+      this.$toast.loading({
+        message: '登录中...',
+        forbidClick: true, // 禁用背景点击
+        duration: 0
+      })
+
 
       try {
         const res = await login(user)
         console.log('登录成功', res)
+        this.$toast.success('登录成功')
       } catch (err) {
-        console.log('登录失败', err)
+        if (err.response.status === 400) {
+          console.log('手机号或者验证码错误', err)
+          this.$toast.fail('手机号或者验证码错误')
+        } else {
+          console.log('登录失败，请稍后重试', err)
+          this.$toast.fail('登录失败，请稍后重试')
+        }
       }
+    },
+    async onSendSms() {
+
     }
   }
 }
